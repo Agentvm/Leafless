5bb2433ce785edaeb96f410964f00ce3809e6b57
agentvm <agentvm@users.noreply.github.com>
2020-02-20 17:47:52 +0000

    Basic Movement and Camera

---
+++ Assets/Scripts/FollowWithOffset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWithOffset : MonoBehaviour
+{
+
+    [SerializeField] private Transform master;
+    private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = master.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = master.position - offset;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Components
+    CharacterController controller;
+
+    // Constants
+    [SerializeField] float movement_speed = 8f;
+
+    //
+    Vector3 input;
+
+    // Damping variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get Components
+        controller = GetComponent<CharacterController> ();
+
+        // no input
+        input.x = 0f;
+        input.y = 0f;
+        input.z = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Use input class please
+        input.x = Input.GetAxis ("Horizontal");
+        input.z = Input.GetAxis ("Vertical");
+
+        controller.Move (input * Time.deltaTime * movement_speed );
+    }
+}
