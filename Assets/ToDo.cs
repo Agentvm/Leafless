564b2b6b1e2c967c286be16ab0eb9a2e8e303090
Agentvm <Agentvm@users.noreply.github.com>
2020-03-18 15:10:42 +0100

    trailing

--- Assets/ToDo.cs
+++ Assets/ToDo.cs
@@ -1,12 +1,9 @@
 /*
 
-    World Creation: What happens on a loop?
     Shoot in State Machine
     Mobile Unterstützung (Touch)
     Sound ausschalten
     Guitar Sound
-    In der Spielszene die Zeit zurücksetzen
-    Prozedural
     Schießen schneller
 
 
