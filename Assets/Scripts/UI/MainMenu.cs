906cd12f2e60a4c60d0783bd5fb7f55f52d3a725
Agentvm <pc.gamer@hotmail.de>
2023-02-01 09:14:19 +0100

    Remedy Toggle Layout

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Leafless.UI
+{
+    public class MainMenu : MonoBehaviourSingleton<MainMenu>
+    {
+        public bool TutorialToggleActive { get => _tutorialToggle.isOn; }
+        [SerializeField] private Toggle _tutorialToggle;
+        /*private Toggle TutorialToggle
+        {
+            get
+            {
+                if (_tutorialToggle == null)
+                    _tutorialToggle = this.transform.Find(nameof(TutorialToggle)).GetComponent<Toggle>();
+
+                return _tutorialToggle;
+            }
+        }*/
+
+        public bool SoundToggleActive { get => _soundToggle.isOn; }
+        [SerializeField] private Toggle _soundToggle;
+        /*private Toggle SoundToggle
+        {
+            get
+            {
+                if (_soundToggle == null)
+                    _soundToggle = this.transform.Find(nameof(SoundToggle)).GetComponent<Toggle>();
+
+                return _soundToggle;
+            }
+        }*/
+
+        void Start ()
+        {
+
+        }
+    }
+}
--- Assets/Scripts/UI/Tutorial/Tutorial.cs
+++ Assets/Scripts/UI/Tutorial/Tutorial.cs
@@ -1,130 +1,112 @@
-﻿using Leafless.UI.Elements.Toggles;
+﻿using Leafless.UI;
 using UnityEngine;
 
-public class Tutorial : MonoBehaviour
+namespace Leafless.UI.Tutorial
 {
-    // Static instance of AudioManager which allows it to be accessed by any other script.
-    public static Tutorial Instance = null;
-    [SerializeField] GameObject move_text;
-    [SerializeField] GameObject leaf_text;
-    [SerializeField] GameObject shoot_text;
-    [SerializeField] GameObject world_text;
-
-    Transform player;
-
-    Vector3 origin;
-    Vector3 text_offset;
-
-    // Start is called before the first frame update
-    void Awake ()
+    public class Tutorial : MonoBehaviourSingleton<Tutorial>
     {
-        // check that there is only one instance of this and that it is not destroyed on load
-        if (Instance == null)
-            Instance = this;
-        else if (Instance != this)
-            Destroy (gameObject);
-        //DontDestroyOnLoad (gameObject);
+        [SerializeField] GameObject move_text;
+        [SerializeField] GameObject leaf_text;
+        [SerializeField] GameObject shoot_text;
+        [SerializeField] GameObject world_text;
 
-        //if ( UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name == "Menu" )
-        //    this.gameObject.SetActive (false);
-        //else this.gameObject.SetActive (true);
-    }
+        Transform player;
 
-    // Start is called before the first frame update
-    void Start ()
-    {
-        player = GameObject.FindWithTag ("Player").transform;
-        origin = player.position;
-        text_offset = move_text.transform.position - player.position;
+        Vector3 origin;
+        Vector3 text_offset;
 
-        if (!TutorialToggleSwitch.State)
-            this.gameObject.SetActive (false);
-        else this.gameObject.SetActive (true);
-    }
-
-    // Update is called once per frame
-    void Update ()
-    {
-
-
-        // tell the player to move
-        if (player && Vector3.Distance (player.position, origin) > 6f)
+        // Start is called before the first frame update
+        void Start()
         {
-            move_text.SetActive (false);
-            leaf_text.SetActive (true);
+            player = GameObject.FindWithTag("Player").transform;
+            origin = player.position;
+            text_offset = move_text.transform.position - player.position;
+
+            if (!MainMenu.Instance.TutorialToggleActive)
+                this.gameObject.SetActive(false);
+            else this.gameObject.SetActive(true);
         }
 
-        // tell the player to eat
-        if (leaf_text.activeSelf && leaf_text.activeInHierarchy)
+        // Update is called once per frame
+        void Update()
         {
-            // get the nearest leaf and place the text above it
-            Vector3 leaf_position = getNearestTagPosition (player.position, "Interactable");
 
-            // if it is too far away, direct the player to it
-            if (Vector3.Distance (player.position, leaf_position) > 18f)
-                leaf_text.transform.position = player.position + (leaf_position - player.position) * 0.35f + text_offset;
-            else
-                leaf_text.transform.position = leaf_position + text_offset;
 
-            // stop if score rises
-            if (GameState.Instance.Award > 2)
+            // tell the player to move
+            if (player && Vector3.Distance(player.position, origin) > 6f)
             {
-                leaf_text.SetActive (false);
-                shoot_text.SetActive (true);
+                move_text.SetActive(false);
+                leaf_text.SetActive(true);
             }
 
-        }
-
-        // tell the player to shoot
-        if (shoot_text.activeSelf && shoot_text.activeInHierarchy)
-        {
-            // get the nearest enemy and place the text above it
-            Vector3 enemy_position = getNearestTagPosition (player.position, "Enemy");
-
-            if (Vector3.Distance (player.position, enemy_position) > 20f)
-                shoot_text.transform.position = player.position + (enemy_position - player.position) * 0.35f + text_offset;
-            else
-                shoot_text.transform.position = enemy_position + text_offset;
-
-            // Stop if score rises
-            if (GameState.Instance.Award > 7)
+            // tell the player to eat
+            if (leaf_text.activeSelf && leaf_text.activeInHierarchy)
             {
-                shoot_text.SetActive (false);
-                world_text.SetActive (true);
-                world_text.transform.position = player.transform.position + text_offset;
-            }
-        }
+                // get the nearest leaf and place the text above it
+                Vector3 leaf_position = getNearestTagPosition("Interactable");
 
-        // show the player how to extend the world
-        if (world_text.activeSelf && world_text.activeInHierarchy)
-        {
+                // if it is too far away, direct the player to it
+                if (Vector3.Distance(player.position, leaf_position) > 18f)
+                    leaf_text.transform.position = player.position + (leaf_position - player.position) * 0.35f + text_offset;
+                else
+                    leaf_text.transform.position = leaf_position + text_offset;
+
+                // stop if score rises
+                if (GameState.Instance.Award > 2)
+                {
+                    leaf_text.SetActive(false);
+                    shoot_text.SetActive(true);
+                }
+
+            }
+
+            // tell the player to shoot
+            if (shoot_text.activeSelf && shoot_text.activeInHierarchy)
+            {
+                // get the nearest enemy and place the text above it
+                Vector3 enemy_position = getNearestTagPosition("Enemy");
+
+                if (Vector3.Distance(player.position, enemy_position) > 20f)
+                    shoot_text.transform.position = player.position + (enemy_position - player.position) * 0.35f + text_offset;
+                else
+                    shoot_text.transform.position = enemy_position + text_offset;
+
+                // Stop if score rises
+                if (GameState.Instance.Award > 7)
+                {
+                    shoot_text.SetActive(false);
+                    world_text.SetActive(true);
+                    world_text.transform.position = player.transform.position + text_offset;
+                }
+            }
+
             // End the Tutorial if world exends
-            if (GameObject.FindGameObjectsWithTag ("Panel").Length > 1)
+            if (world_text.activeSelf && world_text.activeInHierarchy && GameObject.FindGameObjectsWithTag("Panel").Length > 1)
             {
-                this.gameObject.SetActive (false);
+                this.gameObject.SetActive(false);
                 GameState.Instance.tutorial_completed = true;
             }
         }
-    }
 
-    Vector3 getNearestTagPosition (Vector3 at_position, string tag)
-    {
-        GameObject[] leaf_array = GameObject.FindGameObjectsWithTag (tag);
-        Vector3 nearest_pos = player.position;
-        float shortest_distance = 100f;
-
-        foreach (GameObject leaf in leaf_array)
+        Vector3 getNearestTagPosition(string tag)
         {
-            float current_distance = Vector3.Distance (leaf.transform.position, player.position);
+            GameObject[] leaf_array = GameObject.FindGameObjectsWithTag(tag);
+            Vector3 nearest_pos = player.position;
+            float shortest_distance = 100f;
 
-            if (current_distance < shortest_distance)
+            foreach (GameObject leaf in leaf_array)
             {
-                shortest_distance = current_distance;
-                nearest_pos = leaf.transform.position;
+                float current_distance = Vector3.Distance(leaf.transform.position, player.position);
+
+                if (current_distance < shortest_distance)
+                {
+                    shortest_distance = current_distance;
+                    nearest_pos = leaf.transform.position;
+                }
+
             }
 
+            return nearest_pos;
         }
-
-        return nearest_pos;
     }
 }
