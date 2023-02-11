c8c8a8a8dec4ccd7acbf0e5d2b6b2c1cd6242aea
Agentvm <pc.gamer@hotmail.de>
2023-02-11 10:56:54 +0100

    Update ToDo

--- Assets/ToDo.cs
+++ Assets/ToDo.cs
@@ -1,38 +1,48 @@
 /*
 ### Bugs
     
+    Enemies do not bite
     (Low) A point is still scored when eating with a full belly
     (Low) Two Leaves can be eaten at once, if both of them are clicked
+    (Low) Score Text does not disappear
+    
+### Features:
 
-### ToDo:
-
-    Add Sound Toggle
+    Allow to stop eating leaf
+    If enemy and leaf are both targeted in one click, prefer enemy action (suppress eating the leaf)
         
+    Extend Menu to be used in-game (Esc)
+    - Pause
+    - Need to remove the Start Game Button
+
+    Find or make a cool model for the edges of the map
+
+    Improve the feeling of shooting, making it more dynamic
+        
+    Make Touch more reliable by tracking each single touch Instance
+    General Balancing
+        Sparse, giant Plants at maxIntensity, little sprouts at start
+
+    - Add shimmery blue Leaves that act as powerups (flight, pierce shot, reload speed, eating speed)
+    - Award permanent bonuses for fully grazed plants (speed, ammo consumption [+ effect], ) (later marked on the minimap)
+    - Add Wave Events, where one platform is locked and 10 enemies have to be defeated
+        - Started when entering a certain area (later marked on the minimap)
+        - Started when fully grazing a plant
+
+    - Guitar Sound
+    - Make a Minimap with RenderTexture
+        - Add POIs to Minimap
+
 ### Refactoring:
 
+    Add namespaces for everyone :)
     Think twice about using a singleton for GameState, possible expose static readonly properties instead
     Use global variables for the player, either using ScriptableObjects (https://www.youtube.com/watch?v=raQ3iHhE_Kk)
         or using a static base class featuring an extendable dictionary.
         You could also use events to register a player in its start method for everyone who wants to know.
-
-### Features:
-        Find or make a cool model for the edges of the map
-        
-        Make Touch more reliable by tracking each single touch Instance
-        General Balancing
-            Sparse, giant Plants at maxIntensity, little sprouts at start
-
-        - Add shimmery blue Leaves that act as powerups (flight, pierce shot, reload speed, eating speed)
-        - Award permanent bonuses for fully grazed plants (speed, ammo consumption [+ effect], ) (later marked on the minimap)
-        - Add Wave Events, where one platform is locked and 10 enemies have to be defeated
-            - Started when entering a certain area (later marked on the minimap)
-            - Started when fully grazing a plant
-
-        - Guitar Sound
-        - Make a Minimap with RenderTexture
-            - Add POIs to Minimap
     
-### Done:
+
+------ ### Done ### ------
 
         v 1.4 - in development
         Put the credits right
@@ -44,6 +54,7 @@
         Leaves Shader does not reset after MouseOver
         Rework Shooting - strongly increase turn speed after triggering a shot
         Find the right Intensity Curve
+        Added Sound Toggle
 
         v 1.3 - released
         The farther, the more intense
